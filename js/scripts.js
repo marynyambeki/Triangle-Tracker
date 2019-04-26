9d521fdefc1f0426f8ea85b37765e9800f1d39a4
mary <marynyambeki1@gmail.com>
2019-04-26 16:07:52 +0300

    Add Scriptfile

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
-<script>
 
-function Triangle Tracker(){
+
+function Triangle(){
 
 var SideA=parseInt(document.getElementbyId("SideA").value);
 var SideB=parseInt(document.getElementbyId("SideB").value);
 var SideC=parseInt(document.getElementbyId("Sidec").value);
 var answer=document.getElementbyId ('answer');
 
+if(SideA==SideB && SideB==SideC)
+            answer.textcontent("Equilateral");
+
+        else if(SideA >= (SideB+SideC) || SideC >= (SideB+SideA) || SideB >= (SideA+SideC) )
+            answer.textcontent("Not a triangle");
+
+        else if ((SideA==SideB && SideB!=SideC ) || (SideA!=SideB && SideC==SideA) || (SideC==SideB && SideC!=SideA))
+            answer.textcontent("Isosceles");
+
+        else if(SideA!=SidebB && SideB!=SideC && SideC!=SideA)
+            answer.textcontent("Scalene");
+
+}
+}
+
+function Tracker(){
+var SideA=parseInt(document.getElementbyId("SideA").value);
+var SideB=parseInt(document.getElementbyId("SideB").value);
+var SideC=parseInt(document.getElementbyId("Sidec").value);
+var answer=document.getElementbyId ('answer');
 
 }
 
-</script>
