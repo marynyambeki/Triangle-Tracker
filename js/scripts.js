2bdab625f6521d8f789a6b5f53b43f4d1f47e112
mary <marynyambeki1@gmail.com>
2019-04-26 15:20:59 +0300

    Add dimensions

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+<script>
+
+function Triangle Tracker(){
+
+var SideA=parseInt(document.getElementbyId("SideA").value);
+var SideB=parseInt(document.getElementbyId("SideB").value);
+var SideC=parseInt(document.getElementbyId("Sidec").value);
+var answer=document.getElementbyId ('answer');
+
+
+}
+
+</script>
