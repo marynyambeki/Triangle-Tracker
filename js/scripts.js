71d4f2699d59f02a4902ed54f2b4da904095b462
mary <marynyambeki1@gmail.com>
2019-04-29 10:35:37 +0300

    Add changes to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-
 function display() {
-    var sidea = parseInt(document.getElementById('fvalue').value);
-    var sideb = parseInt(document.getElementById('svalue').value);
-    var sidec = parseInt(document.getElementById('tvalue').value);
-   
-    if (sidea+sideb<=sidec||sideb+sidec<=sidea||sidea+sidec<=sideb) {
-      document.getElementById('result').innerHTML="This values do NOT make a triangle";
-    }
-      else if (sidea===sideb && sidea===sidec && sideb===sidec) {
-        document.getElementById('result').innerHTML="This is an equilateral triangle";
-      }
-      else if (sidea!==sideb && sideb!==sidec && sidea!==sidec) {
-        document.getElementById('result').innerHTML="This is a scalene triangle";
-      }
-      else {
-        document.getElementById('result').innerHTML="This is an isosceles triangle";
-      }
+ var sideA = parseInt(document.getElementById('fvalue').value);
+ var sideB= parseInt(document.getElementById('svalue').value);
+ var sideC = parseInt(document.getElementById('tvalue').value);
+
+ if (sideA+sideB<=sideC||sideA+sideC<=sideA||sideA+sideC<=sideB) {
+   document.getElementById('result').innerHTML="This values do NOT make a triangle";
+ }
+   else if (sideA===sideB && sideA===sideC && sideB===sideC) {
+     document.getElementById('result').innerHTML="This is an equilateral triangle";
+   }
+   else if (sideA!==sideB && sideB!==sideC && sideA!==sideC) {
+     document.getElementById('result').innerHTML="This is a scalene triangle";
+   }
+   else {
+     document.getElementById('result').innerHTML="This is an isosceles triangle";
+   }
+}
