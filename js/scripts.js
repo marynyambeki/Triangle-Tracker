60243f40a3c46208111ff91c6c87bba848f35d7f
mary <marynyambeki1@gmail.com>
2019-04-29 10:10:06 +0300

    make changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,18 @@
 
-
-function Triangle(){
-
-var SideA=text(document.getElementbyId("SideA").value);
-var SideB=text(document.getElementbyId("SideB").value);
-var SideC=text(document.getElementbyId("Sidec").value);
-var text=document.getElementbyId ('result');
-
-var sides = ["sideA", "sideB", "sideC", "text"]
-            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA){
-             text = document.getElementById('result').innerHTML = "This is not a triangle!";
-            }
-               else if (sideA === sideB && sideA === sideC && sideB === sideC){
-                 text = document.getElementById('result').innerHTML = "This is an equilateral triangle";
-               }
-               else if (sideA === sideB || sideA === sideC || sideB === sideC){
-                 text = document.getElementById('result').innerHTML = "This is an isosceles triangle";
-               }
-               else if (sideA**2 + sideB**2 === sideC**2){
-                 text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
-               }
-               else if (sideA**2 + sideC**2 === sideB**2){
-                 text = document.getElementById('result').innerHTML = "This is a right-angled triangle.";
-               }
-               else {
-                 text = document.getElementById('result').innerHTML = "This is a scalene triangle.";
-               }
-
+function display() {
+    var sidea = parseInt(document.getElementById('fvalue').value);
+    var sideb = parseInt(document.getElementById('svalue').value);
+    var sidec = parseInt(document.getElementById('tvalue').value);
+   
+    if (sidea+sideb<=sidec||sideb+sidec<=sidea||sidea+sidec<=sideb) {
+      document.getElementById('result').innerHTML="This values do NOT make a triangle";
+    }
+      else if (sidea===sideb && sidea===sidec && sideb===sidec) {
+        document.getElementById('result').innerHTML="This is an equilateral triangle";
+      }
+      else if (sidea!==sideb && sideb!==sidec && sidea!==sidec) {
+        document.getElementById('result').innerHTML="This is a scalene triangle";
+      }
+      else {
+        document.getElementById('result').innerHTML="This is an isosceles triangle";
+      }
